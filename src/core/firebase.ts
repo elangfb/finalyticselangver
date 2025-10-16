587ce4911925df9e1f18999f980975454fc94851
Luthfi Solahudin <luthfisolahudin@outlook.com>
2025-10-16 22:45:07 +0700

    refactor: auto firebase env config

--- src/core/firebase.ts
+++ src/core/firebase.ts
@@ -8,7 +8,15 @@ import { getFunctions } from 'firebase/functions'
 import { getStorage } from 'firebase/storage'
 
 // --- Firebase Config ---
-const firebaseConfig = {
+const DEVELOPMENT_FirebaseConfig = {
+  apiKey: "AIzaSyAT81L_gsi3PWwxiz1FpoiUD54aYYhrkj8",
+  authDomain: "finalytics-development.firebaseapp.com",
+  projectId: "finalytics-development",
+  storageBucket: "finalytics-development.firebasestorage.app",
+  messagingSenderId: "334725384863",
+  appId: "1:334725384863:web:69d07127690da125c630aa"
+}
+const PRODUCTION_FirebaseConfig = {
   apiKey: 'AIzaSyB9_J1AZkSbCM9v3PeV4m33qojHX51bLwg',
   authDomain: 'finalytics-62350.firebaseapp.com',
   projectId: 'finalytics-62350',
@@ -17,8 +25,20 @@ const firebaseConfig = {
   appId: '1:586305419053:web:b94a325fd5b649340305a4',
 }
 
+const knownProductionHostnames = ['finalytics.id', 'www.finalytics.id', 'app.finalytics.id']
+const isProduction = knownProductionHostnames.includes(window.location.hostname)
+const currentFirebaseConfig = isProduction
+  ? PRODUCTION_FirebaseConfig
+  : DEVELOPMENT_FirebaseConfig
+
+console.debug(
+  'Firebase is running in ' +
+  (isProduction ? 'PRODUCTION' : 'DEVELOPMENT') +
+  ' environment.',
+);
+
 // --- Initialize and Export Firebase Services ---
-export const app = initializeApp(firebaseConfig)
+export const app = initializeApp(currentFirebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
 export const functions = getFunctions(app)
